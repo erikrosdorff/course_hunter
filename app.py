e4b1daad87a4ae5d713d683ddac8807d12532012
Erik <erik.rosdorff@gmail.com>
2023-01-23 18:38:42 +0800

    Created delete methods

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 from dp import items, stores
-import uuid
+import uuid 
 from flask_smorest import abort
 
 app = Flask(__name__)
@@ -67,7 +67,7 @@ def create_item():
             message="Bad Request. Ensure 'price', 'store_id', and 'name' are included in the JSON payload. "
         )
     
-    for item in item.values():
+    for item in items.values():
         if(
             item_data["name"] == item["name"]
             and item_data["store_id"] == item["store_id"]
@@ -76,7 +76,7 @@ def create_item():
     if item_data["store_id"] not in stores:
         abort(404, message="Store not found")
     
-    item_id = uuid.uuid().hex
+    item_id = uuid.uuid4().hex
     item = {**item_data, "id" : item_id}
     items[item_id] = item
 
@@ -97,6 +97,37 @@ def get_item(item_id):
         return items[item_id]
     except KeyError:
         abort(404, message="Item not found.")
+
+@app.delete("/item/<string:item_id>")
+def delete_item(item_id):
+    try:
+        del items[item_id]
+        return {"message": "Item deleted"}
+    except KeyError:
+        abort(404, message="Item not found.")
+
+@app.put("/item/<string:item_id>")
+def update_item(item_id):
+    item_data = request.get_json()
+    if "price" not in item_data or "name" not in "item_data":
+        abort(400, message="Bad request, ensure 'price', and 'name' are included in teh JSON payload.")
+
+    try:
+        item = items[item_id]
+        item |= item_data #changes the data in item_data
+
+        return item
+    except KeyError:
+        abort(404, message="Item not found")
+
+@app.delete("/store/<string:store_id>")
+def delete_item(store_id):
+    try:
+        del stores[store_id]
+        return {"message": "Item deleted"}
+    except KeyError:
+        abort(404, message="Item not found.")
+
 
 @app.get("/item")
 def get_all_items():
