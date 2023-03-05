a3350633a6c9e67ac31663b9c97adca42c51e4c9
sharikopodshipnik <sharikopodshipnik@LINX.myguest.virtualbox.org>
2023-03-05 19:04:55 +0800

    SQLALCHEMY

--- schemas.py
+++ schemas.py
@@ -1,15 +1,24 @@
 from marshmallow import Schema, fields
 
+class PlainItemSchemas(Schema):
+    id = fields.Str(dump_only=True)
+    name = fields.Str(required=True)
+    price = fields.Float(required=True)
+
+
+
 class ItemUpdateSchema(Schema):
     name = fields.Str()
     price = fields.Float()
 
-class StoreSchema(Schema):
+class PlainStoreSchema(Schema):
     id = fields.Str(dump_only=True)
     name = fields.Str(required=True)
 
-class ItemSchemas(Schema):
-    id = fields.Str(dump_only=True)
-    name = fields.Str(required=True)
-    price = fields.Float(required=True)
-    store_id = fields.Str(required=True)
+class ItemSchema(PlainItemSchemas):
+    store_id = fields.Int(required = True, load_only = True)
+    store = fields.Nested(PlainStoreSchema(), dump_only=True)
+
+class StoreSchema(PlainStoreSchema):
+    items = fields.List(fields.Nested(PlainItemSchemas()), dunmp_only=True)
+
